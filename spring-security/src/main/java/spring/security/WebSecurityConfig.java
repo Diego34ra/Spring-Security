b370cd2a96bc5a6f64d3ca2d974c7fb3162fa4e2
Diego Ribeiro <diegoribeiro13ra@hotmail.com>
2022-06-15 13:13:16 -0300

    HttpSecurity
    Utilização do HttpSecurity para melhor segurança do sistema

--- spring-security/src/main/java/spring/security/WebSecurityConfig.java
+++ spring-security/src/main/java/spring/security/WebSecurityConfig.java
@@ -1,8 +1,10 @@
 package spring.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -11,6 +13,16 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers(HttpMethod.POST,"/login").permitAll()
+                .antMatchers("/users").hasAnyRole("USERS","ADMIN")
+                .antMatchers("/admin").hasAnyRole("ADMIN")
+                .anyRequest().authenticated().and().formLogin();
+    }
+    
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
