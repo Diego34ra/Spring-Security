000a4da42d70d680315764f5771d00d16a26374c
Diego Ribeiro <diegoribeiro13ra@hotmail.com>
2022-06-14 23:08:29 -0300

    Sistema todo
    Implementação de todo o sistema para aprendizado com Spring Security.

---
+++ spring-security/src/main/java/spring/security/WellcomerController.java
@@ -0,0 +1,26 @@
+package spring.security;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WellcomerController {
+
+    @GetMapping
+    public String welcome(){
+        return "Bem vindo ao meu Spring Boot Web API";
+    }
+
+    @GetMapping("/users")
+    @PreAuthorize("hasAnyRole('ADMIN','USERS')")
+    public String user(){
+        return "Usuario autorizado";
+    }
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasAnyRole('ADMIN')")
+    public String admin(){
+        return "Adminstrador autorizado";
+    }
+}
