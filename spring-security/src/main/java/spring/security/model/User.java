0f090a4d8d8fac5f047e06da95cc69d8c109bda5
Diego Ribeiro <diegoribeiro13ra@hotmail.com>
2022-06-16 00:10:56 -0300

    Commit do User

--- spring-security/src/main/java/spring/security/model/User.java
+++ spring-security/src/main/java/spring/security/model/User.java
@@ -1,7 +1,11 @@
 package spring.security.model;
 
-@Entity
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
+@Entity
+@Table(name = "tab_user")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -14,4 +18,55 @@ public class User {
     @Column(length = 100, nullable = false)
     private String password;
 
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "tab_user_roles", joinColumns = @JoinColumn(name = "user"))
+    @Column(name = "role_id")
+    private List <String> roles = new ArrayList<>();
+
+    public User() {
+    }
+
+    public User(String username){
+        this.username = username;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<String> roles) {
+        this.roles = roles;
+    }
 }
